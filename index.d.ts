f622fa07eb851e1f57b870f0c93e94d53a6ecc81
Pushpendra Upadhyay <pushpendra.languify@gmail.com>
2022-07-15 18:38:54 +0530

    Initial commit

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @param {string} name Name of the person to greet
+ */
+export function greet(name: string): void;
+
+/**
+ * 
+ * @param {string} name Name of the person to do Namaste
+ */
+export function namaste(name: string): void;
---
+++ index.js
@@ -0,0 +1,12 @@
+function greet(name) {
+  console.log(`Hello ${name}..., I'm glad to meet you!`);
+}
+
+function namaste(name) {
+  console.log(`Namaste ${name}..., Aapse milke khushi huyi!`);
+}
+
+module.exports = {
+  greet,
+  namaste,
+};
